ee04ddbc177204ffce47730b3e548d5ee012defd
limeng <lm2343635@126.com>
2016-04-21 14:08:35 +0900

    Initial Commit

---
+++ MuShare-iOS/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  MuShare-iOS
+//
+//  Created by lidaye on 4/21/16.
+//  Copyright © 2016 limeng. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MuShare-iOS/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MuShare-iOS
+//
+//  Created by lidaye on 4/21/16.
+//  Copyright © 2016 limeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
