dce136814179dc10da214afe82197cb4f7396940
limeng <lm2343635@126.com>
2016-08-06 21:32:27 +0900

    Test image upload using STS

--- MuShare-iOS/ProfilePhotoViewController.m
+++ MuShare-iOS/ProfilePhotoViewController.m
@@ -11,6 +11,7 @@
 #import <MobileCoreServices/MobileCoreServices.h>
 #import "AlertTool.h"
 #import "InternetHelper.h"
+#import "DaoManager.h"
 
 @interface ProfilePhotoViewController ()
 
@@ -19,6 +20,8 @@
 @implementation ProfilePhotoViewController {
     UIImagePickerController *imagePickerController;
     AFHTTPSessionManager *manager;
+    DaoManager *dao;
+    User *loginedUser;
 }
 
 - (void)viewDidLoad {
@@ -26,10 +29,94 @@
         NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
     }
     [super viewDidLoad];
-    manager = [InternetHelper getSessionManager:nil];
+    dao = [[DaoManager alloc] init];
+    loginedUser=[dao.userDao getLoginedUser];
+    manager=[InternetHelper getSessionManager: loginedUser.token];
     imagePickerController = [[UIImagePickerController alloc] init];
     imagePickerController.delegate = self;
     
+    
+    [self testUpload];
+}
+
+- (void)testUpload {
+    NSString *endpoint = @"http://oss.mushare.cn";
+    id<OSSCredentialProvider> credential =  [[OSSFederationCredentialProvider alloc] initWithFederationTokenGetter:^OSSFederationToken *{
+        NSURL * url = [NSURL URLWithString:@"http://test.mushare.cn/api/oss/sts/get"];
+        
+        //1.创建request
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        
+        //2.创建一个 NSMutableURLRequest 添加 header
+        NSMutableURLRequest *mutableRequest = [request mutableCopy];
+        [mutableRequest addValue:loginedUser.token forHTTPHeaderField:@"Authorization"];
+        
+        //3.把值覆给request
+        request = [mutableRequest copy];
+        
+        OSSTaskCompletionSource * tcs = [OSSTaskCompletionSource taskCompletionSource];
+        NSURLSession * session = [NSURLSession sharedSession];
+        
+        // 发送请求
+        NSURLSessionTask * sessionTask = [session dataTaskWithRequest:request
+                                                    completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                        if (error) {
+                                                            [tcs setError:error];
+                                                            return;
+                                                        }
+                                                        [tcs setResult:data];
+                                                    }];
+        [sessionTask resume];
+        
+        // 需要阻塞等待请求返回
+        [tcs.task waitUntilFinished];
+        
+        // 解析结果
+        if (tcs.task.error) {
+            NSLog(@"get token error: %@", tcs.task.error);
+            return nil;
+        } else {
+            // 返回数据是json格式，需要解析得到token的各个字段
+            NSDictionary * object = [NSJSONSerialization JSONObjectWithData:tcs.task.result
+                                                                    options:kNilOptions
+                                                                      error:nil];
+            object = [object valueForKey:@"body"];
+            NSLog(@"get token: %@", object);
+            OSSFederationToken * token = [OSSFederationToken new];
+            token.tAccessKey = [object objectForKey:@"accessKeyId"];
+            token.tSecretKey = [object objectForKey:@"accessKeySecret"];
+            token.tToken = [object objectForKey:@"securityToken"];
+            token.expirationTimeInGMTFormat = [object objectForKey:@"expiration"];
+            NSLog(@"get token: %@ %@ %@", token.tAccessKey, token.tSecretKey, token.tToken);
+            return token;
+        }
+    }];
+    
+    OSSClient *client = [[OSSClient alloc] initWithEndpoint:endpoint credentialProvider:credential];
+    
+    
+    OSSPutObjectRequest * put = [OSSPutObjectRequest new];
+    
+    put.bucketName = @"mushare";
+    put.objectKey = @"test15678.jpg";
+    
+    put.uploadingData = UIImageJPEGRepresentation([UIImage imageNamed:@"login-bg.jpg"], 1.0); //UIImageJPEGRepresentation(_profilePhotoImageView.image, 1.0); // 直接上传NSData
+    
+    put.uploadProgress = ^(int64_t bytesSent, int64_t totalByteSent, int64_t totalBytesExpectedToSend) {
+        NSLog(@"%lld, %lld, %lld", bytesSent, totalByteSent, totalBytesExpectedToSend);
+    };
+    
+    OSSTask * putTask = [client putObject:put];
+    
+    [putTask continueWithBlock:^id(OSSTask *task) {
+        if (!task.error) {
+            NSLog(@"upload object success!");
+        } else {
+            NSLog(@"upload object failed, error: %@" , task.error);
+        }
+        return nil;
+    }];
+
 }
 
 #pragma mark - UIImagePickerControllerDelegate
@@ -50,71 +137,7 @@
     
     //Upload image to aliyun OSS
     if(_profilePhotoImageView.image) {
-        NSString *endpoint = @"http://oss.mushare.cn";
-        id<OSSCredentialProvider> credential =  [[OSSFederationCredentialProvider alloc] initWithFederationTokenGetter:^OSSFederationToken *{
-            NSURL * url = [NSURL URLWithString:@"http://localhost:8080/distribute-token.json"];
-            NSURLRequest * request = [NSURLRequest requestWithURL:url];
-            OSSTaskCompletionSource * tcs = [OSSTaskCompletionSource taskCompletionSource];
-            NSURLSession * session = [NSURLSession sharedSession];
-            
-            // 发送请求
-            NSURLSessionTask * sessionTask = [session dataTaskWithRequest:request
-                                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                                                            if (error) {
-                                                                [tcs setError:error];
-                                                                return;
-                                                            }
-                                                            [tcs setResult:data];
-                                                        }];
-            [sessionTask resume];
-            
-            // 需要阻塞等待请求返回
-            [tcs.task waitUntilFinished];
-            
-            // 解析结果
-            if (tcs.task.error) {
-                NSLog(@"get token error: %@", tcs.task.error);
-                return nil;
-            } else {
-                // 返回数据是json格式，需要解析得到token的各个字段
-                NSDictionary * object = [NSJSONSerialization JSONObjectWithData:tcs.task.result
-                                                                        options:kNilOptions
-                                                                          error:nil];
-                OSSFederationToken * token = [OSSFederationToken new];
-                token.tAccessKey = [object objectForKey:@"accessKeyId"];
-                token.tSecretKey = [object objectForKey:@"accessKeySecret"];
-                token.tToken = [object objectForKey:@"securityToken"];
-                token.expirationTimeInGMTFormat = [object objectForKey:@"expiration"];
-                NSLog(@"get token: %@", token);
-                return token;
-            }
-        }];
-                                                 
-        OSSClient *client = [[OSSClient alloc] initWithEndpoint:endpoint credentialProvider:credential];
         
-        
-        OSSPutObjectRequest * put = [OSSPutObjectRequest new];
-        
-        put.bucketName = @"mushare";
-        put.objectKey = @"test-ios";
-        
-        put.uploadingData = UIImageJPEGRepresentation(_profilePhotoImageView.image, 1.0); // 直接上传NSData
-        
-        put.uploadProgress = ^(int64_t bytesSent, int64_t totalByteSent, int64_t totalBytesExpectedToSend) {
-            NSLog(@"%lld, %lld, %lld", bytesSent, totalByteSent, totalBytesExpectedToSend);
-        };
-        
-        OSSTask * putTask = [client putObject:put];
-        
-        [putTask continueWithBlock:^id(OSSTask *task) {
-            if (!task.error) {
-                NSLog(@"upload object success!");
-            } else {
-                NSLog(@"upload object failed, error: %@" , task.error);
-            }
-            return nil;
-        }];
-
     }
 }
 
